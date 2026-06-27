3dcd04ea2a5d19e60ec4743c30a74b0a9f0afffb
A350xt <hkhtzzs@126.com>
2026-06-27 19:31:44 +0800

    bug fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -98,11 +98,6 @@ const config: Config = {
             position: 'left',
         },
         {
-          to: '/app-download',
-          label: 'APP 下载',
-          position: 'left',
-        },
-        {
           href: 'https://github.com/A350xt/Lie-downCraft',
           label: 'GitHub',
           position: 'right',
